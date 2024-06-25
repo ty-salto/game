dcd65a1cb966254be0b43b3bf07b985a5a7d05ea
ty-salto <97984618+ty-salto@users.noreply.github.com>
2024-06-24 19:37:54 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,125 +1,126 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {
+  "use strict";
 
-'use strict';
+  const startButton = document.getElementById("start-btn");
+  const playAgainButton = document.getElementById("again-btn");
+  const cardContainer = document.querySelector(".memory-game");
+  // const memoryCard = document.querySelectorAll('.memory-card');
+  const backFaceImages = ["snow"]; //"snow", "snow","snow", "snow", "snow"?
+  const frontFaceImages = [
+    "ace-of-pentacles",
+    "devil",
+    "empress",
+    "hermit",
+    "star",
+    "three-of-cups",
+  ];
+  const cardsPickList = [...frontFaceImages, ...frontFaceImages];
+  const cardCount = cardsPickList.length;
 
-const startButton = document.getElementById('start-btn');
-const playAgainButton = document.getElementById('again-btn');
-const cardContainer = document.querySelector('.memory-game');
-// const memoryCard = document.querySelectorAll('.memory-card');
-const backFaceImages = ["snow"]; //"snow", "snow","snow", "snow", "snow"?
-const frontFaceImages = ["ace-of-pentacles", "devil", "empress", "hermit", "star", "three-of-cups"];
-const cardsPickList = [...frontFaceImages, ...frontFaceImages]; 
-const cardCount = cardsPickList.length;
+  let revealedCount = 0;
+  let activeCard = null;
+  let waitingEndOfMove = false;
 
-let revealedCount = 0;
-let activeCard = null;
-let waitingEndOfMove = false;
+  playAgainButton.style.display = "none";
 
-playAgainButton.style.display = 'none';
+  startButton.addEventListener("click", startGame); //or this () => {});
 
-startButton.addEventListener("click",startGame); //or this () => {});
-
-function startGame() {
+  function startGame() {
     console.log("Game started");
-    startButton.style.display = 'none';
-    startButton.style.disabled = true;
+    startButton.style.display = "none";
+    playAgainButton.style.display = "block";
+    playAgainButton.style.disabled = false;
+    playAgainButton.onclick = playAgain;
     initializeCards();
-};
+  }
 
-playAgainButton.addEventListener("click",playAgain);
-
- function playAgain() {
+  function playAgain() {
     console.log("Play again");
-    playAgainButton.style.display = 'none';
-    playAgainButton.disabled = true;
     revealedCount = 0;
     activeCard = null;
     waitingEndOfMove = false;
-    startGame();
-    }
+    initializeCards();
+  }
 
-//hide play again
+  //hide play again
 
-
-function initializeCards() {
-    cardContainer.innerHTML = ''; 
-    const shuffledCards = shuffle(cardsPickList); 
+  function initializeCards() {
+    cardContainer.innerHTML = "";
+    const shuffledCards = shuffle(cardsPickList);
     for (let i = 0; i < cardCount; i++) {
-        const card = buildCard(shuffledCards[i]);
-        cardContainer.appendChild(card);
+      const card = buildCard(shuffledCards[i]);
+      cardContainer.appendChild(card);
     }
-}
+  }
 
-function buildCard(card) {
-    const element = document.createElement('div');
-    element.classList.add('memory-card');
-    element.setAttribute('data-card', card);
+  function buildCard(card) {
+    const element = document.createElement("div");
+    element.classList.add("memory-card");
+    element.setAttribute("data-card", card);
 
-    const frontFace = document.createElement('img');
-    frontFace.classList.add('front-face');
-    frontFace.src = `../images/${card}.jpg`; 
+    const frontFace = document.createElement("img");
+    frontFace.classList.add("front-face");
+    frontFace.src = `../images/${card}.jpg`;
 
-    const backFace = document.createElement('img');
-    backFace.classList.add('back-face');
-    backFace.src = '../images/back-face.png';
+    const backFace = document.createElement("img");
+    backFace.classList.add("back-face");
+    backFace.src = "../images/snow.jpg";
 
     element.appendChild(frontFace);
     element.appendChild(backFace);
 
-    element.addEventListener('click', handleCardClick);
+    element.addEventListener("click", handleCardClick);
 
     return element;
-}
+  }
 
-function handleCardClick(event) {
+  function handleCardClick(event) {
     const clickedCard = event.currentTarget;
 
-    if (waitingEndOfMove || clickedCard === activeCard || clickedCard.classList.contains('flip')) {
-        return;
+    if (
+      waitingEndOfMove ||
+      clickedCard === activeCard ||
+      clickedCard.classList.contains("flip")
+    ) {
+      return;
     }
 
-    clickedCard.classList.add('flip');
+    clickedCard.classList.add("flip");
 
     if (!activeCard) {
-        activeCard = clickedCard;
-        return;
+      activeCard = clickedCard;
+      return;
     }
 
     checkForMatch(clickedCard);
-}
+  }
 
-
-function checkForMatch(clickedCard) {
-    const card1 = activeCard.getAttribute('data-card');
-    const card2 = clickedCard.getAttribute('data-card');
+  function checkForMatch(clickedCard) {
+    const card1 = activeCard.getAttribute("data-card");
+    const card2 = clickedCard.getAttribute("data-card");
 
     if (card1 === card2) {
-        revealedCount += 2;
-        activeCard = null;
-        if (revealedCount === cardCount) {
-            setTimeout(() => alert('You won!'), 500);
-        }
+      revealedCount += 2;
+      activeCard = null;
+      if (revealedCount === cardCount) {
+        setTimeout(() => alert("You won!"), 500);
+      }
     } else {
-        waitingEndOfMove = true;
-        setTimeout(() => {
-            activeCard.classList.remove('flip');
-            clickedCard.classList.remove('flip');
-            activeCard = null;
-            waitingEndOfMove = false;
-        }, 1000);
+      waitingEndOfMove = true;
+      setTimeout(() => {
+        activeCard.classList.remove("flip");
+        clickedCard.classList.remove("flip");
+        activeCard = null;
+        waitingEndOfMove = false;
+      }, 1000);
     }
-}
+  }
 
-function shuffle(array) {
+  function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-}
+  }
 });
-
-
-
-
-
